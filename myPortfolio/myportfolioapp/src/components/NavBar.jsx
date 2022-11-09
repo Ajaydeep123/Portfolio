cae72c7fac1fe36d8131360fd191f4595e948230
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2022-11-10 01:56:38 +0530

    :construction: bug corrected, installed right tail

---
+++ myPortfolio/myportfolioapp/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const NavBar = () => {
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+        <div>
+        <h1>Ajaydeep</h1>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
